4caac638bcbd777deec3724411983a3eeca0e582
Xavier-Nourry <xavier.nourry@etu.univ-savoie.fr>
2021-03-19 10:27:45 +0100

    working soap server/service

--- DeliveryCosts-SOAP_Service/service.py
+++ DeliveryCosts-SOAP_Service/service.py
@@ -1,28 +1,28 @@
-    import logging
-    logging.basicConfig(level=logging.DEBUG)
-    from spyne import Application, rpc, ServiceBase, \
-        Integer, Unicode
-    from spyne import Iterable
-    from spyne.protocol.soap import Soap11
-    from spyne.server.wsgi import WsgiApplication
+import logging
+logging.basicConfig(level=logging.DEBUG)
+from spyne import Application, rpc, ServiceBase, \
+Integer, Unicode, Float # TODO : enlever imports inutiles
+from spyne import Iterable
+from spyne.protocol.soap import Soap11
+from spyne.server.wsgi import WsgiApplication
 
-    class HelloWorldService(ServiceBase):
-        @rpc(Unicode, Integer, _returns=Iterable(Unicode))
-        def say_hello(ctx, name, times):
-            for i in range(times):
-                yield 'Hello, %s' % name
+class DeliveryCostsService(ServiceBase):
+    @rpc(Float, Float, _returns=Float)
+    def deliveryCost(ctx, distance, weight):
+        return distance * weight / 100 # TODO : voir si calcul plus réaliste
+        
 
-    application = Application([HelloWorldService],
-        tns='spyne.examples.hello',
-        in_protocol=Soap11(validator='lxml'),
-        out_protocol=Soap11()
-    )
-    
-    if __name__ == '__main__':
-        # You can use any Wsgi server. Here, we chose
-        # Python's built-in wsgi server but you're not
-        # supposed to use it in production.
-        from wsgiref.simple_server import make_server
-        wsgi_app = WsgiApplication(application)
-        server = make_server('0.0.0.0', 8000, wsgi_app)
-        server.serve_forever()
+application = Application([DeliveryCostsService],
+    tns='DeliveryCosts_SOAP-Service', # TODO : voir utilité
+    in_protocol=Soap11(validator='lxml'),
+    out_protocol=Soap11()
+)
+
+if __name__ == '__main__':
+    # You can use any Wsgi server. Here, we chose
+    # Python's built-in wsgi server but you're not
+    # supposed to use it in production.
+    from wsgiref.simple_server import make_server
+    wsgi_app = WsgiApplication(application)
+    server = make_server('127.0.0.1', 8000, wsgi_app)
+    server.serve_forever()
